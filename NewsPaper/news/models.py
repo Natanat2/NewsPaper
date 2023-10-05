c0681871679fe438680c1deb2847078578169f74
Natanat2 <natanat2@mail.ru>
2023-10-06 00:25:37 +0600

    git init

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+    authorUser = models.OneToOneField(User, on_delete = models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default = 0)
+
+    def update_rating(self):
+        postRating = self.
+
+
+class Category(models.Model):
+    name = models.CharField(unique = True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+    categoryType = models.CharField(max_length = 2, choices = CATEGORY_CHOICES)
+    dateCreation = models.DateTimeField(auto_now_add = True)
+    postCategory = models.ManyToManyField(Category, through = 'PostCategory')
+    title = models.CharField(null = False)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default = 0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[0:123] + '...'
+
+
+class PostCategory(models.Model):
+    postTrough = models.ForeignKey(Post, on_delete = models.CASCADE)
+    categoryTrough = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+
+class Comment(models.Model):
+    commentPost = models.ForeignKey(Post, on_delete = models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add = True)
+    rating = models.SmallIntegerField(default = 0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()

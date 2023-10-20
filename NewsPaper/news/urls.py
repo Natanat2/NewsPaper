034c8bd4c6e94d270570e6fde6c778badecc123c
Natanat2 <natanat2@mail.ru>
2023-10-21 00:14:43 +0600

    D4.7. Итоговый проект

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import NewsList, News, Search
+from .views import NewsList, News, Search, NewsCreate, NewsEdit, NewsDelete, ArticlesCreate, ArticlesEdit, ArticlesDelete
 
 urlpatterns = [
-   path('', NewsList.as_view()),
-   path('<int:pk>', News.as_view()),
-   path('search', Search.as_view()),
+   path('news/', NewsList.as_view(), name='news_list'),
+   path('news/<int:pk>', News.as_view(), name='news_detail'),
+   path('news/search', Search.as_view()),
+   path('news/create', NewsCreate.as_view(), name='news_create'),
+   path('news/<int:pk>/edit/', NewsEdit.as_view(), name='news_update'),
+   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+   path('articles/create', ArticlesCreate.as_view(), name='articles_create'),
+   path('articles/<int:pk>/edit/', ArticlesEdit.as_view(), name='articles_update'),
+   path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='articles_delete'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 from .filters import PostFilter
+from .forms import PostForm
 
 
 class NewsList(ListView):
@@ -20,10 +22,12 @@ class NewsList(ListView):
         context['filterset'] = self.filterset
         return context
 
+
 class News(DetailView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
+
 
 class Search(ListView):
     model = Post
@@ -39,3 +43,49 @@ class Search(ListView):
         context = super().get_context_data(**kwargs)
         context['filterset'] = self.filterset
         return context
+
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_create.html'
+
+    def form_valid(self, form):
+        Post = form.save(commit = False)
+        Post.categoryType = 'NW'
+        return super().form_valid(form)
+
+
+class NewsEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('news_list')
+
+
+class ArticlesCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'articles_create.html'
+
+    def form_valid(self, form):
+        Post = form.save(commit = False)
+        Post.categoryType = 'AR'
+        return super().form_valid(form)
+
+
+class ArticlesEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'articles_edit.html'
+
+
+class ArticlesDelete(DeleteView):
+    model = Post
+    template_name = 'articles_delete.html'
+    success_url = reverse_lazy('news_list')

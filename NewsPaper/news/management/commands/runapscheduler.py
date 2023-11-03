f101e5fd1e5779f6d04d63e82c356229d47e3726
Natanat2 <natanat2@mail.ru>
2023-11-03 17:10:03 +0600

    ЗАДАНИЕ 6.7.1 100%

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -1,29 +1,55 @@
-import logging
-
+from datetime import datetime, timedelta
+from django.utils import timezone
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
-from django.core.mail import mail_managers
 from django.core.management.base import BaseCommand
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
-from django_apscheduler.models import DjangoJobExecution
+from news.models import Post, Category, Subscriber
+from django.core.mail import EmailMultiAlternatives
 
-from news.models import Post
+def send_weekly_article_list():
+    # Получите текущую дату
+    current_date = datetime.now()
 
-logger = logging.getLogger(__name__)
+    # Найдите последнюю дату отправки для задачи
+    last_execution = DjangoJobExecution.objects.filter(job__id='send_weekly_article_list').last()
 
+    # Если последняя дата выполнения существует, используйте ее
+    if last_execution:
+        last_execution_date = last_execution.run_etime.astimezone(timezone(settings.TIME_ZONE))
+    else:
+        last_execution_date = current_date - timedelta(weeks=1)
 
-def my_job():
-    products = Post.objects.order_by('dateCreation')
-    text = '\n'.join(['{} - {}'.format(p.name, p.price) for p in products])
-    mail_managers("Самые дешевые товары", text)
+    # Найдите все статьи, которые были опубликованы после последней отправки
+    new_articles = Post.objects.filter(dateCreation__gt=last_execution_date)
 
+    # Сгруппируйте статьи по категориям
+    articles_by_category = {}
+    for article in new_articles:
+        for category in article.postCategory.all():
+            articles_by_category.setdefault(category, []).append(article)
 
-@util.close_old_connections
-def delete_old_job_executions(max_age=604_800):
-    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+    # Отправьте список статей подписчикам категорий
+    for category, articles in articles_by_category.items():
+        subscribers = Subscriber.objects.filter(category=category)
+        if subscribers:
+            subject = f'Новые статьи в категории {category.name}'
+            text_content = 'Новые статьи:\n\n'
+            for article in articles:
+                text_content += f'{article.title}\n{article.get_absolute_url()}\n\n'
 
+            for subscriber in subscribers:
+                msg = EmailMultiAlternatives(subject, text_content, None, [subscriber.user.email])
+                msg.send()
+
+    # Обновите последнюю дату выполнения задачи
+    if last_execution:
+        last_execution.run_etime = current_date
+        last_execution.save()
+    else:
+        DjangoJobExecution(job_id='send_weekly_article_list', run_etime=current_date).save()
 
 class Command(BaseCommand):
     help = "Runs APScheduler."
@@ -33,13 +59,13 @@ class Command(BaseCommand):
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
         scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(minute="20", hour="10"),
-            id="my_job",  # The `id` assigned to each job MUST be unique
+            send_weekly_article_list,
+            trigger=CronTrigger(day_of_week="fri", hour="18"),
+            id="send_weekly_article_list",
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added weekly job: 'send_weekly_article_list'.")
 
         scheduler.add_job(
             delete_old_job_executions,

36ca0fa8184d4fd78116b904587d2b4fe04a38d9
Natanat2 <natanat2@mail.ru>
2023-11-08 22:36:22 +0600

    Задание 7.5 100%

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -7,13 +7,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
-
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'send-weekly-newsletter': {
         'task': 'NewsPaper.tasks.send_weekly_article_list',
-        'schedule': crontab(hour=8, day_of_week='mon'),
+        'schedule': crontab(day_of_week = 'mon', hour = '8'),
         'options': {'queue': 'weekly-news'},
     },
 }
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -9,22 +9,23 @@ from django.utils import timezone
 from news.models import Post, Category, Subscriber
 from django.core.mail import EmailMultiAlternatives
 
+
 @shared_task()
 def send_weekly_article_list():
     # Получите текущую дату
     current_date = datetime.now()
 
     # Найдите последнюю дату отправки для задачи
-    last_execution = DjangoJobExecution.objects.filter(job__id='send_weekly_article_list').last()
+    last_execution = DjangoJobExecution.objects.filter(job__id = 'send_weekly_article_list').last()
 
     # Если последняя дата выполнения существует, используйте ее
     if last_execution:
         last_execution_date = last_execution.run_etime.astimezone(timezone(settings.TIME_ZONE))
     else:
-        last_execution_date = current_date - timedelta(weeks=1)
+        last_execution_date = current_date - timedelta(weeks = 1)
 
     # Найдите все статьи, которые были опубликованы после последней отправки
-    new_articles = Post.objects.filter(dateCreation__gt=last_execution_date)
+    new_articles = Post.objects.filter(dateCreation__gt = last_execution_date)
 
     # Сгруппируйте статьи по категориям
     articles_by_category = {}
@@ -34,7 +35,7 @@ def send_weekly_article_list():
 
     # Отправьте список статей подписчикам категорий
     for category, articles in articles_by_category.items():
-        subscribers = Subscriber.objects.filter(category=category)
+        subscribers = Subscriber.objects.filter(category = category)
         if subscribers:
             subject = f'Новые статьи в категории {category.name}'
             text_content = 'Новые статьи:\n\n'
@@ -50,15 +51,15 @@ def send_weekly_article_list():
         last_execution.run_etime = current_date
         last_execution.save()
     else:
-        DjangoJobExecution(job_id='send_weekly_article_list', run_etime=current_date).save()
+        DjangoJobExecution(job_id = 'send_weekly_article_list', run_etime = current_date).save()
 
 
 @shared_task
 def process_post_category_changed(post_id):
-    instance = Post.objects.get(pk=post_id)
-    category_names = instance.postCategory.all().values_list('name', flat=True)
+    instance = Post.objects.get(pk = post_id)
+    category_names = instance.postCategory.all().values_list('name', flat = True)
     category_names_str = ', '.join(category_names)
-    subscribers = User.objects.filter(subscriptions__category__in=instance.postCategory.all()).distinct()
+    subscribers = User.objects.filter(subscriptions__category__in = instance.postCategory.all()).distinct()
 
     subject = f'Вышла новая новость в категории {category_names_str}'
     text_content = (

4a798e1afc636ca6d3036baeca65cd11154c7ea7
Natanat2 <natanat2@mail.ru>
2023-12-08 22:38:48 +0600

    D11. add field and filters

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'categoryType', 'dateCreation', 'title', 'text', 'rating')
+    list_filter = ('author', 'categoryType', 'rating')
+    search_fields = ('title', 'text')
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
 
